3555fc3688011c33741e5e147640efdbac8950f5
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-05 20:15:52 -0800

    adding tailwind css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,13 +12,22 @@ export default function Navbar() {
                     className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ben-Jam-In
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink to="/post" 
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700"
+                    >
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/project">
+                    <NavLink to="/project" 
+                    className=" inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to="about">
+                    <NavLink to="/about" 
+                    className=" inline-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700"
+                    >
                         About ME!
                     </NavLink>
                 </nav>
