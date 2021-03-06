db4a07a60267deb38cb0b250429fcbc3520b1081
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-05 19:35:53 -0800

    working on Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,12 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
     return (
         <header className="bg-red-600">
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink to="/" 
+                    exact 
+                    activeClassName="text-white"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest">
                         Ben-Jam-In
                     </NavLink>
                     <NavLink to="/post">
