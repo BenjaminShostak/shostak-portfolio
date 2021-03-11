743ff486ea4853a001fc91e863bfb97963360273
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-10 23:07:44 -0800

    added post.js logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import image from "../HD-Summer-Desktop-Wallpaper.jpg";
 export default function Home() {
     return(
         <main>
-            <img src={image} alt="Wild Fire" className="absolute object-cover w-full h-full z-index:1"/>
+            <img src={image} alt="Wild Fire" className="absolute object-cover w-full h-full"/>
                 <section className="realtive flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                    <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >Hi There! I'm Ben.
+                    <h1 className="text-5xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >Hi There! I'm Ben.
                     </h1>
                 </section>
         </main>
