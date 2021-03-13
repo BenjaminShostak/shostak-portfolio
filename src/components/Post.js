7aa5915bec590a54c427570ac7fa7a671509b578
Benjamin Shostak <benshostak@rocketmail.com>
2021-03-13 11:51:29 -0800

    added Link to post.js page from react router dom

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+
 
 export default function Post() {
     const [postData, setPost] = useState(null);
@@ -28,13 +30,26 @@ export default function Post() {
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
                 <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Blog Page</h2>
-                <div>
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {postData && postData.map((post,index) => (
                     <article>
-                        <Link>
-                            <span></span>
+                        <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+                            <span className="block h-64 realtive rounded shadow leading-snug bg-white border-l-8 border-green-400"
+                            key={index}>
+                                <img src={post.mainImage.asset.url}
+                                alt={post.mainImage.asset.url}
+                                className="w-full h-full rounded-r object-cover absolute"
+                                 />
+
+                            </span>
+                            <span className="block relative h-full flex justify-end items-end pr-4 pb-4"></span>
+                            <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded">
+                                {post.title}
+                            </h3>
                         
                         </Link>
                     </article>
+                    ))}
                 </div>
             </section>
         </main>
