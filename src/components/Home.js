a5212131a51dfa69e70a7417f673b3e63b952f10
Benjamin Shostak <benshostak@rocketmail.com>
2021-05-05 17:11:18 -0700

    changed relative to absolute, rendered text on home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     return(
         <main>
             <img src={image} alt="Wild Fire" className="absolute object-cover w-full h-full"/>
-                <section className="realtive flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                    <h1 className="text-5xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >Hi There! I'm Ben.
+                <section className="absolute flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                    <h1 className="text-5xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name" >Hi There! I'm Ben!
                     </h1>
                 </section>
         </main>
